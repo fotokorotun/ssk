5c9233ffd19e3f3816d01b4f4b30212b14c0e4c4
admin CIFO <admin CIFO@DESKTOP-90KPIC7>
2021-06-11 12:31:12 +0200

    commit 1

---
+++ employee1/src/main/java/employee1/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package employee1;
+
+
+
+	import org.springframework.data.repository.CrudRepository;
+
+	public interface EmployeeRepository extends CrudRepository<Employee,Integer> {
+		
+	}
+	
+	
+
